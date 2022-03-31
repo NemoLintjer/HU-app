73407b06c2d1bc68092a2935e7784e5ee4802c51
NemoLintjer <91617460+NemoLintjer@users.noreply.github.com>
2022-03-31 14:19:03 +0200

    Create Personeel.java

---
+++ Personeel.java
@@ -0,0 +1,10 @@
+public class Personeel{
+  private String naam;
+  private String email;
+  private String wachtwoord;
+  private int personeelsnummer;
+  
+  public null geefAbsenties(){
+  }
+  public null bekijkRooster(){
+  }

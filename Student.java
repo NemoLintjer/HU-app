1509a1fc578540ca646f9b6ad93fc0fa4ca2d6f1
NemoLintjer <91617460+NemoLintjer@users.noreply.github.com>
2022-03-31 14:02:29 +0200

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,16 @@
+public class Student{
+  private String naam;
+  private String adres;
+  priate int studentnummer;
+  private String email;
+  private String wachtwoord;
+  
+  public boolean meldAbsent(){
+  }
+  public boolean meldZiek(){
+  }
+  public null geefAbsenties(){
+  }
+  public null bekijkRooster(){
+  }
+}
